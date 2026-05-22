0e42775f7091c199fb1c8466488f9fc483d68f63
SerbCSharp <SerbCSharp@mail.ru>
2026-05-22 16:50:55 +0300

    22_05_26

--- Cost/Domain/CashFlow.cs
+++ Cost/Domain/CashFlow.cs
@@ -6,13 +6,13 @@
         public string Number { get; set; }
         public DateOnly Date { get; set; }
         public decimal Payment { get; set; }
+        public decimal Receipt { get; set; }
         public string Liter { get; set; }
         public string CostItem { get; set; }
         public string PaymentPurpose { get; set; }
         public string Contractor { get; set; }
         public string TypeOfActivity { get; set; }
         public string AreaOfActivity { get; set; }
-        public decimal Receipt { get; set; }
         public string ContractId { get; set; }
         public string TypeOperation { get; set; }
         public decimal SumTypeOfActivity { get; set; }
